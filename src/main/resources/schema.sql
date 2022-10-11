b7a95f32af280da9ebe93973ccab3dd3e1a06ad2
4e65746f <ediwaldo.neto@outlook.com>
2022-10-11 20:16:14 -0300

    project evolution

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,33 @@
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `email` varchar(75) NOT NULL,
+  `name` varchar(75) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `role` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY (`email`)
+) ENGINE = INNODB;
+
+CREATE TABLE `request` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `creation_date` datetime NOT NULL,
+  `description` text DEFAULT NULL,
+  `state` varchar(12) NOT NULL,
+  `subject` varchar(75) NOT NULL,
+  `owner_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`owner_id`) REFERENCES user(id)
+) ENGINE = INNODB;
+
+CREATE TABLE `request_stage` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `description` text DEFAULT NULL,
+  `realization_date` datetime NOT NULL,
+
+  `state` varchar(12) NOT NULL,
+  `owner_id` bigint(20) NOT NULL,
+  `request_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`owner_id`) REFERENCES user(id),
+  FOREIGN KEY (`request_id`) REFERENCES request(id)
+) ENGINE = INNODB;

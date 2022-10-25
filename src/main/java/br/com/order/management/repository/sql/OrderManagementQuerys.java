8458deb004dc4c68179227ceff16c858bf878e8c
4e65746f <ediwaldo.neto@outlook.com>
2022-10-25 09:44:18 -0300

    fix query

--- src/main/java/br/com/order/management/repository/sql/OrderManagementQuerys.java
+++ src/main/java/br/com/order/management/repository/sql/OrderManagementQuerys.java
@@ -9,7 +9,7 @@ public class OrderManagementQuerys {
             " (email, name, password, `role`) " +
             " VALUES(:email, :name, :password, :role)";
     public static final String FIND_ALL = "SELECT * FROM `user` u";
-    public static final String SEARCH_BY_ID = "SELECT * fROM `user` u WHERE id = :id;";
+    public static final String SEARCH_BY_ID = "SELECT * fROM `user` u WHERE id = :id";
     public static final String UPDATE_USER = "UPDATE `user` SET email = :email, name :name, password = :password, role = :role WHERE id = :id ";
 
 }
